df04d637044f59542eb4972f23b99f45b721262d
cyrozap <cyrozap@gmail.com>
2015-04-07 23:47:08 -0400

    Added comments

--- display_hex_byte.v
+++ display_hex_byte.v
@@ -22,45 +22,62 @@ module display_hex_byte(
 	output wire [2:0] segments_enable
 	);
 
-	parameter refresh_rate = 1000;
-	parameter sys_clk_freq = 100000000;
+	/* These are the module's configurable parameters */
+	parameter refresh_rate = 1000;      // Refresh rate in Hz
+	parameter sys_clk_freq = 100000000; // Input clock frequency in Hz
 
+	/* Pre-calculate the clock divider */
 	localparam clk_divider = sys_clk_freq / (refresh_rate * 3);
 
+	/* 32 bits should be enough for any reasonable clock/freq combo */
 	reg [31:0] divider;
 
+	/*
+	 * Link the registers to their respective output wires, but invert
+	 * them since the physical display segment LEDs are active low.
+	 */
 	reg [7:0] segments_out;
 	reg [2:0] segments_enable_out;
 	assign segments = ~segments_out;
 	assign segments_enable = ~segments_enable_out;
 
+	/* Declare the wires the segment data will be output to */
 	wire [7:0] high_segments;
 	wire [7:0] low_segments;
 
+	/* Instantiate the nibble to segments converters */
 	nibble_to_segments high_nib(hex_byte[7:4], high_segments);
 	nibble_to_segments low_nib(hex_byte[3:0], low_segments);
 
 	always @(posedge clk) begin
 		if (divider < clk_divider)
+			/* Increment the counter by one to count the delay cycles */
 			divider <= divider + 1;
 		else begin
+			/* Reset the counter (divider) */
 			divider <= 0;
+
+			/*
+			 * This case statement cycles between the three 7-segment displays.
+			 * Each case describes the state of the next cycle, updating each
+			 * the displays from left to right.
+			 */
 			case (segments_enable_out)
 				3'b001: begin
-					segments_out <= 8'b00101110;
-					segments_enable_out <= 3'b100;
+					segments_out <= 8'b00101110;   // 'h'
+					segments_enable_out <= 3'b100; // The left display
 				end
 				3'b100: begin
-					segments_out <= high_segments;
-					segments_enable_out <= 3'b010;
+					segments_out <= high_segments; // The high nibble
+					segments_enable_out <= 3'b010; // The center display
 				end
 				3'b010: begin
-					segments_out <= low_segments;
-					segments_enable_out <= 3'b001;
+					segments_out <= low_segments;  // The low nibble
+					segments_enable_out <= 3'b001; // The right display
 				end
 				default: begin
-					segments_out <= 8'h00;
-					segments_enable_out <= 3'b001;
+					segments_out <= 8'h00;         // Turn all segments off
+					segments_enable_out <= 3'b001; // The right display
 				end
 			endcase
 		end
@@ -72,9 +89,17 @@ module nibble_to_segments(
 	output wire [7:0] segments
 	);
 
+	/*
+	 * Store the segment information in segments_out and send that signal
+	 * through the "segments" wire.
+	 */
 	reg [7:0] segments_out;
 	assign segments = segments_out;
 
+	/*
+	 * This section is essentially a lookup table that maps hex digits to
+	 * active display segments.
+	 */
 	always begin
 		case (nibble)
 			4'h0: segments_out = 8'b11111100;
